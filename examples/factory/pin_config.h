77f279d5547a3314079010808fd18a85d20ee335
Vantablack001 <101949514+Vantablack001@users.noreply.github.com>
2024-02-17 11:30:32 +0300

    Update pin_config.h
    a

--- examples/factory/pin_config.h
+++ examples/factory/pin_config.h
@@ -3,8 +3,8 @@
 // #define WIFI_SSID                    "Your-ssid"
 // #define WIFI_PASSWORD                "Your-password"
 
-#define WIFI_SSID                    "Your-ssid"
-#define WIFI_PASSWORD                "Your-password"
+#define WIFI_SSID                    "TP-Link_A6A6"
+#define WIFI_PASSWORD                "Yurtal01."
 
 #define WIFI_CONNECT_WAIT_MAX        (30 * 1000)
 
